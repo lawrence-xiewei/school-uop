25ddd87f824254c2d38cf3998f6f1271ad5235ba
Xie Wei <lawrence.xw86@gmail.com>
2019-09-21 21:56:22 +0800

    first version, GUI done.

---
+++ src/easyprinting/TShirtClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easyprinting;
+
+// 1 = rouan
+/**
+ *
+ * @author XIE WEI and LEO
+ */
+public class TShirtClass {
+    private int color;
+    private int type;
+}
